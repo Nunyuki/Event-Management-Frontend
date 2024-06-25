210d198e4464b1569d62f84a85b78da3d0d6b6ac
Nunyuki <cdaielodie@gmail.com>
2024-06-26 00:21:18 +0200

    fix npm bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { EventPageComponent } from './event-page/event-page.component';
 
 import { UserService } from './services/user.service';
 import { AllCategoryEventComponent } from './all-category-event/all-category-event.component';
@@ -16,14 +17,14 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { CategoryService } from './services/category.service';
 import { EventService } from './services/event.service';
 import { CreateEventComponent } from './create-event/create-event.component';
+import { RegistrationService } from './services/registration.service';
 
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
-import { EventPageComponent } from './event-page/event-page.component';
-import { RegistrationService } from './services/registration.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,7 @@ import { RegistrationService } from './services/registration.service';
     AllCategoryEventComponent,
     MainPageComponent,
     CreateEventComponent,
-    EventPageComponent,
+    EventPageComponent
   ],
   imports: [
     BrowserModule,
@@ -52,7 +53,8 @@ import { RegistrationService } from './services/registration.service';
     CategoryService,
     EventService,
     RegistrationService,
-    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' } 
+    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' },
+    provideAnimationsAsync() 
   ],
   bootstrap: [AppComponent]
 })
